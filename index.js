f58850a6ad8969f6017704bb9b01fdb700dca9a3
Yosmarli Parica <101285117+Yosmarli@users.noreply.github.com>
2023-01-19 13:23:55 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const lista = [
+  "Yosmarli",
+  26,
+  true,
+  new Date("1996,11,06").toLocaleDateString("ve-VE"),
+  {
+    titulo: "El coronel no tiene quien le escriba",
+    autor: "Gabriel García Márquez",
+    fecha: new Date("1961, 04, 13").toLocaleDateString("ve-VE"),
+    url: "https://www.amazon.com/-/es/Gabriel-Garc%C3%ADa-M%C3%A1rquez/dp/0307475441/ref=sr_1_12?keywords=gabriel+garcia+marquez&qid=1674147013&s=books&sprefix=gabriel+%2Cstripbooks-intl-ship%2C120&sr=1-12",
+  },
+];
